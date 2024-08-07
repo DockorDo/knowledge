96d28853637bff59d0ca45142fb8b36d0e52e3d7
v_runzhiguo <v_runzhiguo@tencent.com>
2024-08-07 18:54:09 +0800

    手写promise及其方法完整版

--- js/promise/myPromise.js
+++ js/promise/myPromise.js
@@ -6,7 +6,7 @@ const PROMISE_STATUS={
 }
 
 let errorMsg;
-class myPromise{
+class MyPromise{
     constructor(executor){
         errorMsg = []
         // 边界判断
@@ -23,42 +23,286 @@ class myPromise{
 
         this.PromiseState = PROMISE_STATUS.PROMISE_PENDDING;
         this.PromiseResult = undefined;
+        this.reason = undefined;
+
+        this.fulfilledList = [];
+        this.rejectedList = [];
+        this.finallyList = [];
         try{
-            executor(myPromise.resolve.bind(this),myPromise.reject.bind(this))
+            executor(MyPromise._resolve.bind(this),MyPromise._reject.bind(this))
         } catch(error) {
-            throw error;
+            if(this.rejectedList.length){
+                MyPromise.reject.bind(this)   
+            }else throw error;
         }
     }
 
-    then(onFulfilled,onRejected){}
+    then(onFulfilled,onRejected){
+        return new MyPromise((resolve,reject)=>{
+            if(this.PromiseState === PROMISE_STATUS.PROMISE_FULFILLED){
+                try{
+                    resolve(typeof onFulfilled==="function" ? onFulfilled(this.PromiseResult) : PromiseResult)
+                }catch(err){
+                    reject(err.message)
+                }
+            }
+             if(this.PromiseState === PROMISE_STATUS.PROMISE_REJECTED){
+                try{
+                    resolve(typeof onRejected==="function" ? onRejected(this.PromiseResult) : PromiseResult)
+                }catch(err){
+                    reject(err.message)
+                }
+            }
+          
+            if(this.PromiseState === PROMISE_STATUS.PROMISE_PENDDING){
+                this.fulfilledList.push(()=>{
+                    try {
+                        resolve(typeof onFulfilled === "function"?onFulfilled(this.PromiseResult):this.PromiseResult)
+                    } catch (error) {
+                        reject(error.message)
+                    }
+                })
+                this.rejectedList.push(()=>{
+                    try {
+                        resolve(typeof onRejected === "function"?onRejected(this.PromiseResult):this.PromiseResult)
+                    } catch (error) {
+                        reject(error.message)
+                    }
+                })
+            }
+        })
+    }
 
-    catch(){}
+    catch(onRejected){
+        return new MyPromise((resolve,reject)=>{
+            if(this.PromiseState === PROMISE_STATUS.PROMISE_PENDDING){
+                this.rejectedList.push(()=>{
+                    try {
+                        resolve(typeof onRejected==="function"?onRejected(this.PromiseResult):this.PromiseResult)
+                    } catch (err) {
+                        throw err;
+                    }
+                })
+            }
+            if(this.PromiseState === PROMISE_STATUS.PROMISE_REJECTED){
+                try{
+                    this.rejectedList.push(()=>{
+                        resolve(typeof onRejected === "function"?onRejected(this.PromiseResult):undefined)
+                    })
+                } catch(err){
+                    throw err
+                }
+            }
+        })
+    }
 
-    finally(){}
-
-    static all(){}
-
-    static race(){}
-
-    static any(){}
-
-    static allSettled(){}
-
-    static withResolvers(){}
-
-    static resolve(val){
+    finally(onFinallyCB){
         if(this.PromiseState === PROMISE_STATUS.PROMISE_PENDDING){
-            this.PromiseState = PROMISE_STATUS.PROMISE_FULFILLED;
+            this.finallyList.push(onFinallyCB);
         }
-
-    }
-
-    static reject(){
-        if(this.PromiseState === PROMISE_STATUS.PROMISE_PENDDING){
-            this.PromiseState = PROMISE_STATUS.PROMISE_REJECTED;
+        if([PROMISE_STATUS.PROMISE_FULFILLED,PROMISE_STATUS.PROMISE_REJECTED].includes(this.PromiseState)){
+            onFinallyCB();
         }
     }
 
+    static all(iteratorObj){
+        if([undefined,null].includes(iteratorObj)||typeof iteratorObj[Symbol.iterator] !== 'function'){
+            switch(iteratorObj){
+                case undefined:throw new TypeError("undefined is not iterable (cannot read property Symbol(Symbol.iterator))")
+                case null: throw new TypeError("object null is not iterable (cannot read property Symbol(Symbol.iterator))")
+                default:throw new TypeError(`${typeof iteratorObj} is not iterable (cannot read property Symbol(Symbol.iterator))`)
+            }
+        }
+        return new MyPromise((resolve,reject)=>{
+            const arr = [];
+            for(let i =0;i<iteratorObj.length;i++){
+                iteratorObj[i].then((res)=>{
+                    arr[i] = res;
+                },
+                (reson) => {
+                    reject(reson)
+                })
+            };
+            resolve(arr)
+        })
+    }
+
+    static race(iteratorObj){
+        if([undefined,null].includes(iteratorObj)||typeof iteratorObj[Symbol.iterator] !== 'function'){
+            switch(iteratorObj){
+                case undefined:throw new TypeError("undefined is not iterable (cannot read property Symbol(Symbol.iterator))")
+                case null: throw new TypeError("object null is not iterable (cannot read property Symbol(Symbol.iterator))")
+                default:throw new TypeError(`${typeof iteratorObj} is not iterable (cannot read property Symbol(Symbol.iterator))`)
+            }
+        }
+        return new MyPromise((resolve,reject)=>{
+            for(let i = 0;i<iteratorObj.length;i++){
+                iteratorObj[i].then((res)=>{ 
+                    resolve(res)},(reson)=>reject(reson))
+            }
+        })
+    }
+
+    static any(iteratorObj){
+        if([undefined,null].includes(iteratorObj)||typeof iteratorObj[Symbol.iterator] !== 'function'){
+            switch(iteratorObj){
+                case undefined:throw new TypeError("undefined is not iterable (cannot read property Symbol(Symbol.iterator))")
+                case null: throw new TypeError("object null is not iterable (cannot read property Symbol(Symbol.iterator))")
+                default:throw new TypeError(`${typeof iteratorObj} is not iterable (cannot read property Symbol(Symbol.iterator))`)
+            }
+        }
+        return new MyPromise((resolve,reject)=>{
+            let val = []
+            for(let i = 0;i<iteratorObj.length;i++){
+                iteratorObj[i].then((res)=>{
+                    resolve(res);
+                    
+                },(reson)=>{
+                    val[i] = reson
+                })
+            }
+            if(!iteratorObj.length||!val.length) reject(new Error("All promises were rejected"));
+        })
+    }
+
+    static allSettled(iteratorObj){
+        if([undefined,null].includes(iteratorObj)||typeof iteratorObj[Symbol.iterator] !== 'function'){
+            switch(iteratorObj){
+                case undefined:throw new TypeError("undefined is not iterable (cannot read property Symbol(Symbol.iterator))")
+                case null: throw new TypeError("object null is not iterable (cannot read property Symbol(Symbol.iterator))")
+                default:throw new TypeError(`${typeof iteratorObj} is not iterable (cannot read property Symbol(Symbol.iterator))`)
+            }
+        }
+        return new MyPromise((resolve,reject)=>{
+        try{
+            const res = []
+            for(let i = 0;i<iteratorObj.length;i++){
+                iteratorObj[i].then(result=>{
+                    res[i] = result;
+                },
+                reson=>{
+                    res[i] = reson
+                })
+            }
+            if(res.length == iteratorObj.length){
+                
+                resolve(res)
+            }
+        }catch(err){
+            reject(err)
+        }
+    })
 }
 
-export default myPromise;
+    static withResolvers(){
+        let resolve,reject, promise = new MyPromise((_resolve,_reject)=>{
+            resolve=_resolve;
+            reject=_reject;
+        })
+        return {promise,resolve,reject}
+    }
+
+    static _resolve(val,lastPromise = this){
+        if(val instanceof MyPromise){
+            const status = val.PromiseState;
+            const statusMap = {
+                "pendding":()=>{
+                    lastPromise.PromiseState = "pendding";
+                },
+                "fulfilled":()=>{
+                    resolve(val.PromiseResult,this)
+                },
+                "rejected":()=>{
+                    MyPromise.reject.call(this,val.PromiseResult,this)
+                }
+            }
+            statusMap[status]();
+            return;
+        }
+        if(lastPromise.PromiseState === PROMISE_STATUS.PROMISE_PENDDING){
+            lastPromise.PromiseState = PROMISE_STATUS.PROMISE_FULFILLED;
+            lastPromise.PromiseResult = val;
+        }
+        if(lastPromise.PromiseState === PROMISE_STATUS.PROMISE_FULFILLED){
+            queueMicrotask(()=>{
+                Array.prototype.forEach.call(this.fulfilledList,fn => fn())
+            })
+        }
+    }
+
+    static _reject(reason, lastPromise = this){
+        if(lastPromise.PromiseState === PROMISE_STATUS.PROMISE_PENDDING){
+            lastPromise.PromiseState = PROMISE_STATUS.PROMISE_REJECTED;
+            lastPromise.PromiseResult = reason;
+            lastPromise.reason = reason;
+        }
+        if(lastPromise.PromiseState === PROMISE_STATUS.PROMISE_REJECTED){
+            queueMicrotask(()=>{
+            if(this.rejectedList.length) Array.prototype.forEach.call(this.rejectedList,fn => fn());
+            else if(!this.rejectedList.length){
+                this.PromiseResult='rejected';
+            } 
+        })
+        }
+    }
+
+    static resolve(val){
+        return new MyPromise((resolve,reject)=> resolve(val))
+    }
+    static reject(reason){
+        return new MyPromise((resolve,reject)=> reject(reason))
+    }
+}
+
+const p = new MyPromise((resolve,reject)=>{
+    resolve("123")
+})
+
+
+p.then(res => {
+    console.log(res);
+    return 333
+}).then(res=>{
+    console.log("???:",res)
+    throw new Error("test")
+}).catch(err => {
+    console.log(err)
+}).finally(()=>{
+    console.log("finally")
+})
+
+p.then(res=>{
+    console.log(res)
+})
+
+const allPromise = MyPromise.all([new MyPromise((res,rej)=>{rej(11111)})]);
+console.log(allPromise)
+allPromise.then((res)=>{console.log(res)},(err)=>{console.log("1233333:",err)})
+
+const racePromise = MyPromise.race([new MyPromise((res,rej)=>{setTimeout(()=>{res("race")},1000)}),new MyPromise((res,rej)=>{
+    setTimeout(()=>{
+        rej("race:reject")
+    },2000)
+})])
+
+racePromise.then(res=>{console.log(res)},err=>{console.log(err)})
+
+const anyPromise = MyPromise.any([new MyPromise((resolve,reject)=>{
+    reject("error")
+}),new MyPromise((resolve)=>{
+    setTimeout(()=>{
+        resolve("success")
+    },5000)
+})])
+
+anyPromise.then(res=>{
+    console.log(res)
+})
+
+const allSettledPromise = MyPromise.allSettled([new MyPromise((res)=>res("allsettle:success")),new MyPromise((res,rej)=>rej("allsettle:failed"))])
+
+allSettledPromise.then((res=>{
+    console.log(res)
+}))
+
+export default MyPromise;
--- js/promise/test/promiseA+.spec.ts
+++ js/promise/test/promiseA+.spec.ts
@@ -1,9 +1,9 @@
-import myPromise from "../myPromise";
+import MyPromise from "../myPromise";
 import { describe, test, expect, vi } from "vitest"
 
 
 describe('Terminology 术语规范', () => {
-    const promise = new myPromise();
+    const promise = new MyPromise();
     test("promise是一个具有then方法的对象",()=>{
         expect(typeof promise.then).toBe("function");
     });
@@ -16,44 +16,44 @@ describe('Terminology 术语规范', () => {
     });
     test("value是任何合法的值",async ()=>{
         for(const itm of [undefined,123,"string",{},[]]){
-          await expect(new myPromise((resolve,reject)=>{
+          await expect(()=>(new MyPromise((resolve,reject)=>{
                 resolve(itm)
-              })).rejects.not.toThrowError()
+              }))).not.toThrowError()
         }
     })
 
     test("如果发生异常则使用throw抛出",async()=>{
         const errMsg ="test";
-        await expect(()=>(new myPromise(()=>{
+        await expect(()=>(new MyPromise(()=>{
             throw new Error(errMsg)
         }))).toThrowError()
     })
 
     test("“reason”是一个表示承诺被拒绝的原因的值。", async ()=>{
-        const p = new myPromise((resolve, reject)=>{
+        const p = new MyPromise((resolve, reject)=>{
             reject("test")
         });
-        await expect(p).rejects.toThrow("test");
+        expect(p.reason).toBe("test");
     })
 })
 
 describe("Requirements 要求",()=>{
     test("promise必须处于pendding、fulfilled、rejected之一",()=>{
-        const p = new myPromise(()=>{});
+        const p = new MyPromise(()=>{});
         expect(p.PromiseState).toBe('pendding');
     });
     test("promise的状态可以被转变",()=>{
-        const p = new myPromise((resolve, reject)=>{
+        const p = new MyPromise((resolve, reject)=>{
             resolve("test")
         });
         expect(p.PromiseState).toBe("fulfilled");
     });
     test("当状态从pendding改变后就不能再改变了",()=>{
-        const p1 = new myPromise((resolve,reject)=>{
+        const p1 = new MyPromise((resolve,reject)=>{
             resolve("test");
             reject(123)
         })
-        const p2 = new myPromise((resolve,reject)=>{
+        const p2 = new MyPromise((resolve,reject)=>{
             reject("test")
             resolve("test");
         });
@@ -64,7 +64,7 @@ describe("Requirements 要求",()=>{
 
 describe('then 方法相关', () => { 
     test("promise必须提供then方法来访问其当前或最终的值或原因",()=>{
-        const p = new myPromise(()=>{});
+        const p = new MyPromise(()=>{});
         expect(p.then).not.toBe(undefined);
     });
     test("then方法有两个可选参数",async ()=>{
@@ -74,17 +74,17 @@ describe('then 方法相关', () => {
         }
         const onFulfilledSpy = vi.spyOn(Fn,"onFulfilled");
         const onRejectedSpy = vi.spyOn(Fn,"onFulfilled");
-        const p1 = new myPromise((resolve,reject)=>{resolve()});
-        await p1.then(Fn.onFulfilled,Fn.onRejected);
+        const p1 = new MyPromise((resolve,reject)=>{resolve()});
+        await p1.then(Fn.onFulfilled,Fn.onRejected)
         await expect(onFulfilledSpy).toHaveBeenCalled()
 
-        const p2 = new myPromise((resolve,reject)=>{reject()});
+        const p2 = new MyPromise((resolve,reject)=>{reject()});
         await p2.then(Fn.onFulfilled,Fn.onRejected);
         await expect(onRejectedSpy).toHaveBeenCalled()
     })
 
     test("then方法可以针对一个promise多次调用",()=>{
-        const p = new myPromise();
+        const p = new MyPromise();
 
         const thenSpy= vi.spyOn(p,"then")
         p.then();
@@ -95,11 +95,29 @@ describe('then 方法相关', () => {
     })
 
     test("then 方法返回值也是一个promise",()=>{
-        const p = new myPromise((resolve,reject)=>{
+        const p = new MyPromise((resolve,reject)=>{
             resolve()
         });
         const thenVal = p.then();
-        expect(thenVal.constructor).toBe(myPromise)
+        expect(thenVal.constructor).toBe(MyPromise)
 
     })
  })
+
+ describe('The Promise Resolution Procedure 承诺解决程序', () => { 
+    test("如果promise在确定状态时引用了一个新的promise，它会尝试采用新promise的状态为最终状态",()=>{
+        const p = new MyPromise((resolve,reject)=>{
+            resolve(new MyPromise((resolve,reject)=>{
+                reject("test")
+            }))
+        });
+        expect(p.PromiseState).toBe('rejected')
+
+        const p1 = new MyPromise((resolve,reject)=>{
+            resolve(new MyPromise(()=>{}))
+        })
+        expect(p1.PromiseState).toBe("pendding")
+
+    });
+    
+  })
